df30cb33cf7ca999a79d4db46c058b60739bdc24
Quan Pham <doraemonfanclub@gmail.com>
2018-12-25 17:06:42 +0700

    Added Product model

--- lib/src/models/product.dart
+++ lib/src/models/product.dart
@@ -1,6 +1,34 @@
 import 'package:dart_json_mapper/json_mapper.dart';
 
+enum Type {
+  game
+}
+
 class Product {
   String appName;
-  List<String> achievements;
+  String type;
+  int steamAppId;
+  bool isFree;
+  String detailedDescription;
+  String aboutTheGame;
+  String shortDescription;
+
+  Product.fromJson(Map<String, dynamic> json):
+    appName = json['app_name'],
+    type = json['type'],
+    steamAppId = json['steam_app_id'],
+    isFree = json['is_free'],
+    detailedDescription = json['detailed_description'],
+    aboutTheGame = json['about_the_game'],
+    shortDescription = json['short_description'];
+
+  Map<String, dynamic> toJson() => {
+    'app_name': appName,
+    'type': type,
+    'steam_app_id': steamAppId,
+    'is_free': isFree,
+    'detailed_description': detailedDescription,
+    'about_the_game': aboutTheGame,
+    'short_description': shortDescription
+  };
 }
