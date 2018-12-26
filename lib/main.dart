bffb2dc3dea9a68acd0fc1855c004a3a95aef85c
Quan Pham <doraemonfanclub@gmail.com>
2018-12-26 16:54:09 +0700

    Added cupertino scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:steamclient/src/views/home.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:steamclient/src/views/screens/storefront.dart';
 
 void main() => runApp(SteamClientApp());
 
 class SteamClientApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: "Steam Client",
+    return new CupertinoApp(
       debugShowCheckedModeBanner: false,
-      theme: new ThemeData(
-          primarySwatch: Colors.red,
-          primaryColor: Colors.black,
-          primaryIconTheme: IconThemeData(color: Colors.black),
-          primaryTextTheme: TextTheme(
-              title: TextStyle(color: Colors.black, fontFamily: "Aveny")),
-          textTheme: TextTheme(title: TextStyle(color: Colors.black))),
-      home: new Home(),
+      home: new StoreFront()
     );
   }
 }
--- lib/src/views/screens/storefront.dart
+++ lib/src/views/screens/storefront.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 class StoreFront extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return null;
+    final topBar = Text("MMM");
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        
+      ),
+      child: Container(
+        decoration: BoxDecoration(color: Colors.blueGrey),
+      ),
+    );
   }
 }
