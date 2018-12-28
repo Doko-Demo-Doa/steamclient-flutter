3f925f1aa8dcab059c28d3a6a599d5c7c4472d6f
Quan Pham <doraemonfanclub@gmail.com>
2018-12-28 18:16:20 +0700

    Added search input

--- lib/src/views/screens/storefront.dart
+++ lib/src/views/screens/storefront.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:steamclient/src/views/widgets/storefront/featured_content.dart';
 import 'package:steamclient/src/views/widgets/storefront/carousel.dart';
+import 'package:steamclient/src/views/widgets/hex_color.dart';
 
 class StoreFront extends StatelessWidget {
   @override
@@ -13,12 +14,21 @@ class StoreFront extends StatelessWidget {
             text: 'Steam'
           ),
         ),
-        backgroundColor: Colors.red,
+        backgroundColor: HexColor('171a21'),
       ),
       child: Container(
         decoration: BoxDecoration(color: Colors.blueGrey),
         child: new Column(
           children: <Widget>[
+            Material(
+              borderRadius: BorderRadius.all(Radius.circular(6)),
+              child: TextField(
+                onTap: () {
+                  print('On tap');
+                },
+                decoration: InputDecoration(hintText: 'Lalaland', border: InputBorder.none, contentPadding: const EdgeInsets.all(20.0))
+              ),
+            ),
             Flexible(child: new FeaturedContent()),
             Flexible(child: new Carousel())
           ],
