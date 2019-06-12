e40614436490847532afd80d42698eb759b34314
Quan Pham <doraemonfanclub@gmail.com>
2019-06-13 02:49:13 +0700

    Added the ability to open drawer programmatically

--- lib/views/screens/profile.dart
+++ lib/views/screens/profile.dart
@@ -3,17 +3,25 @@ import 'package:steamclient/views/widgets/hex_color.dart';
 import '../widgets/profile/profile_main.dart';
 import '../widgets/profile/comment_section.dart';
 
-class Profile extends StatelessWidget {
+class ProfileWidgetState extends State<Profile> {
+  GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       drawer: Drawer(
         child: null,
       ),
       appBar: new AppBar(
         centerTitle: true,
         title: new Image.network('https://steamcommunity-a.akamaihd.net/public/shared/images/responsive/header_logo.png', height: 28),
-        leading: Icon(Icons.menu, color: Colors.white),
+        leading: new GestureDetector(
+          onTap: () => {
+            _scaffoldKey.currentState.openDrawer()
+          },
+          child: Icon(Icons.menu, color: Colors.white),
+        ),
         backgroundColor: HexColor('171A21'),
       ),
       body: Flex(
@@ -25,4 +33,10 @@ class Profile extends StatelessWidget {
       ),
     );
   }
+}
+
+
+class Profile extends StatefulWidget {
+  @override
+  ProfileWidgetState createState() => new ProfileWidgetState();
 }
