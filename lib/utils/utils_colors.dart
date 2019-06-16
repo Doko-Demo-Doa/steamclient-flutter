fa29ba5c59d684521ad6b87b44edd814cc00cfb3
Quan Pham <doraemonfanclub@gmail.com>
2019-06-16 23:11:17 +0700

    Refactored comment list

--- lib/utils/utils_colors.dart
+++ lib/utils/utils_colors.dart
@@ -2,4 +2,7 @@ import 'package:steamclient/views/widgets/hex_color.dart';
 
 class CommonColors {
   static var COLOR_SECONDARY_TEXT = HexColor('#BDBDBD');
+  static var COLOR_SLATE = HexColor('#536C7B');
+  static var COLOR_AVOCADO = HexColor('#8FB93B');
+  static var COLOR_COOL_GRAY = HexColor('#9DA3A8');
 }
