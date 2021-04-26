7f7d82c328efe89c85fa59d7d61de9eea7a83fe1
Quan Pham <doraemonfanclub@gmail.com>
2021-04-26 14:52:01 +0700

    Refactor login screen

--- lib/common/predefined_colors.dart
+++ lib/common/predefined_colors.dart
@@ -3,12 +3,14 @@ library predefined_colors;
 import 'package:steamclient/views/widgets/hex_color.dart';
 
 class AppColors {
+  static HexColor ALUMINIUM = HexColor('#98989B');
   static HexColor ALMOST_BLACK = HexColor('#03141E');
   static HexColor AVOCADO = HexColor('#8FB93B');
   static HexColor COAL = HexColor('#393c43');
   static HexColor COOL_GRAY = HexColor('#9DA3A8');
   static HexColor BLUE = HexColor('#198cff');
   static HexColor DARK = HexColor('#2E3136');
+  static HexColor TUNGSTEN = HexColor('#2F3237');
   static HexColor DARK_BLUE = HexColor('#15243C');
   static HexColor DARK_BLUE_GREY = HexColor('#18364E');
   static HexColor DARK_GREY_BLUE = HexColor('#2A4652');
--- lib/views/screens/login.dart
+++ lib/views/screens/login.dart
@@ -1,138 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:provider/provider.dart';
 import 'package:steamclient/common/predefined_colors.dart';
-import 'package:steamclient/vms/auth_vm.dart';
 
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        padding: const EdgeInsets.symmetric(vertical: 42, horizontal: 50),
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [AppColors.DARK_BLUE, AppColors.ALMOST_BLACK],
-          ),
-        ),
+        width: double.infinity,
+        color: AppColors.TUNGSTEN,
+        padding: const EdgeInsets.only(top: 42),
         child: Column(
           children: <Widget>[
             Container(
               margin: const EdgeInsets.fromLTRB(30, 46, 30, 38),
-              decoration: BoxDecoration(boxShadow: [
-                BoxShadow(
-                  color: AppColors.DARK_BLUE,
-                  offset: Offset(0.0, 0.0),
-                  blurRadius: 5.0,
-                  spreadRadius: 7.0,
-                )
-              ], shape: BoxShape.circle),
               child: Icon(
                 FontAwesomeIcons.steam,
-                color: AppColors.DARK_BLUE_GREY,
-                size: 86,
+                color: AppColors.WHITE,
+                size: 160,
+              ),
+            ),
+            Text(
+              "STEAM",
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: AppColors.WHITE,
+              ),
+            ),
+            Text(
+              "Mobile",
+              style: TextStyle(
+                fontSize: 26,
+                color: AppColors.ALUMINIUM,
               ),
             ),
             SizedBox(
               child: Column(
-                children: <Widget>[
-                  TextField(
-                    style: TextStyle(color: Colors.white60),
-                    textInputAction: TextInputAction.done,
-                    keyboardType: TextInputType.phone,
-                    decoration: InputDecoration(
-                      counterText: '',
-                      disabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: AppColors.POWDER_BLUE),
-                      ),
-                      border: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                          color: AppColors.POWDER_BLUE,
-                          width: 1,
-                        ),
-                      ),
-                      hintText: 'Username',
-                      hintStyle: TextStyle(color: AppColors.GUNMETAL),
-                    ),
-                    maxLength: 32,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 10),
-                  ),
-                  TextField(
-                    obscureText: true,
-                    style: TextStyle(color: Colors.white60),
-                    enabled: false,
-                    decoration: InputDecoration(
-                        counterText: '',
-                        disabledBorder: UnderlineInputBorder(
-                            borderSide:
-                                BorderSide(color: AppColors.POWDER_BLUE)),
-                        enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: AppColors.GUNMETAL)),
-                        border: UnderlineInputBorder(
-                            borderSide:
-                                BorderSide(color: AppColors.POWDER_BLUE)),
-                        hintText: 'Password',
-                        hintStyle: TextStyle(color: AppColors.GUNMETAL)),
-                    maxLength: 32,
-                  ),
-                  ConstrainedBox(
-                    constraints: BoxConstraints(minWidth: 120, maxWidth: 230),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: <Widget>[
-                        Padding(
-                            padding: const EdgeInsetsDirectional.only(top: 62)),
-                        TextButton(
-                          style: TextButton.styleFrom(
-                            primary: AppColors.DARK_BLUE_GREY,
-                          ),
-                          onPressed: () {
-                            Provider.of<AuthViewModel>(context, listen: false)
-                                .login(null, null);
-                          },
-                          child: Text(
-                            'Login',
-                            style: TextStyle(color: Colors.white54),
-                          ),
-                        ),
-                        Padding(
-                            padding: const EdgeInsetsDirectional.only(top: 16)),
-                        TextButton(
-                          style: TextButton.styleFrom(
-                            primary: AppColors.LIGHT_NAVY_BLUE,
-                          ),
-                          onPressed: () {},
-                          child: Text(
-                            'Signup',
-                            style: TextStyle(color: Colors.white54),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsetsDirectional.only(top: 42),
-                        ),
-                        TextButton(
-                          style: TextButton.styleFrom(
-                            primary: Colors.transparent,
-                          ),
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          child: Text(
-                            'Go Back',
-                            style: TextStyle(color: Colors.white54),
-                          ),
-                        ),
-                      ],
-                    ),
+                children: [
+                  IconButton(
+                    icon: FaIcon(FontAwesomeIcons.steam),
+                    onPressed: () {},
                   ),
                 ],
               ),
             ),
+            TextField(),
           ],
         ),
       ),
